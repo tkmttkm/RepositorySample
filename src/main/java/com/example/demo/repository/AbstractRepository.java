d823612aa198231a6c2f0922ca0e4dfb1753c7eb
takumi <takumi@DESKTOP-FFTLVNP>
2024-08-12 03:22:46 +0900

    change StringBuilder

--- src/main/java/com/example/demo/repository/AbstractRepository.java
+++ src/main/java/com/example/demo/repository/AbstractRepository.java
@@ -1,10 +1,18 @@
 package com.example.demo.repository;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import com.example.demo.entity.Entity;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,23 +30,26 @@ public abstract class AbstractRepository<T, S> implements IRepository<T> {
 
 	@Autowired
 	protected NamedParameterJdbcTemplate namedjdbcTemplate;
-	
-	/** {@link #findAll()}や{@link #findById(Object)}で取得したデータを格納 */
-	private List<T> dataList; 
 
+	/** {@link #findAll()}や{@link #findById(Object)}で取得したデータを格納 */
+	private List<T> dataList;
+
+	/** エンティティクラス */
+	protected final T entity;
 	/** <pre>
 	 * {@link RowMapper}を実装したクラスを型に持つ
 	 * {@link #findAll()}や{@link #findById(Object)}のマッピングで使用
 	 * </pre> */
 	protected final S rowMapper;
-	
+
+
 	/** {@link #findAll()}や{@link #findById(Object)}で取得したデータ数 */
 	@SuppressWarnings("unused")
 	private int findCount;
-	
+
 	/** {@link #save(List)}や{@link #save(Object)}で保存したデータ数 */
 	private int saveCount;
-	
+
 	/** {@link #deleteById(Object)}で削除したデータ数 */
 	private int deleteCount;
 
@@ -61,13 +72,162 @@ public abstract class AbstractRepository<T, S> implements IRepository<T> {
 	public void setDeleteCount(int deleteCount) {
 		this.deleteCount = deleteCount;
 	}
-	
+
 	public List<T> getDataList() {
 		return dataList;
 	}
 
 	public void setDataList(List<T> dataList) {
 		this.dataList = dataList;
+	}
+	/**
+	 * エンティティのフィールド名を{@code List<String>}にして返す
+	 * @return フィールド名をつめた{@code List<String>}
+	 * @throws Exception
+	 */
+	protected List<String> getFieldNames() throws Exception {
+		try {
+			List<String> list = new ArrayList<>();
+			Field[] fields = entity.getClass().getDeclaredFields();
+			for (Field field : fields) {
+				//privateフィールドへのアクセスを可能にする
+				field.setAccessible(true);
+				// staticフィールドの場合はスキップする
+				if (Modifier.isStatic(field.getModifiers())) {
+					continue;
+				}
+				list.add(field.getName());
+			}
+
+			return list;
+		} catch (IllegalArgumentException e) {
+			System.err.println(e.getMessage() + "\r\n" + e.getStackTrace());
+			throw e;
+		}
+	}
+
+	/**
+	 * エンティティのstaticフィールド以外のフィールド名とその値を{@code Map<String, Object>}として返す
+	 * @param entity
+	 * @return key:フィールド名 value:そのフィールドの値 の{@code Map<String, Object>}
+	 * @throws Exception
+	 */
+	protected Map<String, Object> getFieldNameAndValue(Entity entity) throws Exception {
+		try {
+			Map<String, Object> map = new LinkedHashMap<>();
+
+			Field[] fields = entity.getClass().getDeclaredFields();
+			for (Field field : fields) {
+				//privateフィールドへのアクセスを可能にする
+				field.setAccessible(true);
+				//staticフィールドの場合はスキップする
+				if (Modifier.isStatic(field.getModifiers())) {
+					continue;
+				}
+
+				String name = field.getName();
+				Object value = field.get(entity);
+				map.put(name, value);
+			}
+
+			return map;
+		} catch (IllegalArgumentException | IllegalAccessException e) {
+			System.err.println(e.getMessage() + "\r\n" + e.getStackTrace());
+			throw e;
+		}
+	}
+
+	/**
+	 * <pre>
+	 * key名 = VALUES(:key名), key名 = VALUES(:key名), .....
+	 * を作成する
+	 * </pre>
+	 * @param sql 作成中のSQL文
+	 * @param fieldNameAndValue key:カラム名 value:更新したい値
+	 */
+	protected void setDuplicateUpdateSqlByMap(StringBuilder sql, Map<String, Object> fieldNameAndValue) {
+		List<String> updateSqlList = new ArrayList<>();
+		for (var entrySet : fieldNameAndValue.entrySet()) {
+			updateSqlList.add(entrySet.getKey() + " = VALUES(" + entrySet.getKey() + ")");
+		}
+		sql.append(String.join(", ", updateSqlList));
+	}
+
+	/**
+	 * <pre>
+	 * key名 = VALUES(:key名), key名 = VALUES(:key名), .....
+	 * を作成する
+	 * </pre>
+	 * @param sql 作成中のSQL文
+	 * @param fieldNameAndValue key:カラム名 value:更新したい値
+	 * @throws Exception
+	 */
+	protected void setDuplicateUpdateSqlByMap(StringBuilder sql) throws Exception {
+		List<String> updateSqlList = new ArrayList<>();
+		for (String fieldName : getFieldNames()) {
+			updateSqlList.add(fieldName + " = VALUES(" + fieldName + ")");
+		}
+		sql.append(String.join(", ", updateSqlList) + " ");
+	}
+
+	/**
+	 * <pre>
+	 * (key名, key名, key名, ...) VALUES (:key名, :key名, .....)
+	 * を作成する
+	 * </pre>
+	 * @param sql 作成中のSQL文
+	 * @param fieldNameAndValue key:カラム名 value:その値 のMap<String, Object> {@link #getFieldNameAndValue(Entity)}を使用して作成
+	 * @param params 作成中のパラメター
+	 */
+	protected void setInsertSqlByMap(StringBuilder sql, Map<String, Object> fieldNameAndValue,
+			MapSqlParameterSource params) {
+		String[] columnArray = new String[fieldNameAndValue.size()];
+		String[] valueArray = new String[fieldNameAndValue.size()];
+
+		int index = 0;
+		for (var entrySet : fieldNameAndValue.entrySet()) {
+			String setKey = entrySet.getKey();
+			int idx = 0;
+			while (params.hasValue(setKey)) {
+				setKey = setKey + Integer.valueOf(idx).toString();
+				idx++;
+			}
+			params.addValue(setKey, entrySet.getValue());
+			columnArray[index] = entrySet.getKey();
+			valueArray[index] = ":" + setKey;
+			index++;
+		}
+
+		sql.append("( " + String.join(", ", columnArray) + " ) VALUES ( " + String.join(", ", valueArray) + " ) ");
+	}
+
+	/**
+	 * <pre>
+	 * (key名, key名, key名, ...) VALUES (:key名, :key名, .....)
+	 * を作成する
+	 * </pre>
+	 * @param sql 作成中のSQL文
+	 * @throws Exception
+	 */
+	protected void setInsertSqlByMap(StringBuilder sql) throws Exception {
+		List<String> fieldNames;
+		try {
+			fieldNames = getFieldNames();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw e;
+		}
+		String[] columnArray = new String[fieldNames.size()];
+		String[] valueArray = new String[fieldNames.size()];
+
+		int index = 0;
+		for (String fieldName : fieldNames) {
+			columnArray[index] = fieldName;
+			valueArray[index] = ":" + fieldName;
+			index++;
+		}
+
+		sql.append("( " + String.join(", ", columnArray) + " ) VALUES ( " + String.join(", ", valueArray) + " ) ");
 	}
 
 }
--- src/main/java/com/example/demo/repository/extend/Repository.java
+++ src/main/java/com/example/demo/repository/extend/Repository.java
@@ -1,9 +1,5 @@
 package com.example.demo.repository.extend;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -30,20 +26,20 @@ import com.example.demo.repository.AbstractRepository;
 public class Repository extends AbstractRepository<Entity, EntityRowMapper> {
 
 	public Repository() {
-		super(new EntityRowMapper());
+		super(new Entity(), new EntityRowMapper());
 	}
 
 	@Override
 	public List<Entity> findAll() throws DataAccessException {
-		List<String> select = new ArrayList<>();
+		StringBuilder select = new StringBuilder();
 
-		select.add("SELECT");
-		select.add("*");
-		select.add("FROM");
-		select.add(Entity.TABLE_NAME);
+		select.append("SELECT ");
+		select.append("* ");
+		select.append("FROM ");
+		select.append(Entity.TABLE_NAME);
 
 		try {
-			setDataList(namedjdbcTemplate.query(String.join(" ", select), new EmptySqlParameterSource(), rowMapper));
+			setDataList(namedjdbcTemplate.query(select.toString(), new EmptySqlParameterSource(), rowMapper));
 			return getDataList();
 		} catch (DataAccessException e) {
 			System.err.println(e.getMessage() + "\r\n" + e.getStackTrace());
@@ -53,20 +49,21 @@ public class Repository extends AbstractRepository<Entity, EntityRowMapper> {
 
 	@Override
 	public Optional<Entity> findById(Object id) throws DataAccessException {
-		List<String> select = new ArrayList<>();
+		StringBuilder select = new StringBuilder();
 
-		select.add("SELECT");
-		select.add("*");
-		select.add("FROM");
-		select.add(Entity.TABLE_NAME);
-		select.add("WHERE");
-		select.add(Entity.ID + " = :" + Entity.ID);
+		select.append("SELECT ");
+		select.append("* ");
+		select.append("FROM ");
+		select.append(Entity.TABLE_NAME);
+		select.append(" WHERE ");
+		select.append(Entity.ID
+				+ " = :" + Entity.ID);
 
 		MapSqlParameterSource params = new MapSqlParameterSource();
 		params.addValue(Entity.ID, id);
 
 		try {
-			setDataList(namedjdbcTemplate.query(String.join(" ", select), params, rowMapper));
+			setDataList(namedjdbcTemplate.query(select.toString(), params, rowMapper));
 
 			if (getFindCount() == 0) {
 				return Optional.ofNullable(null);
@@ -82,19 +79,19 @@ public class Repository extends AbstractRepository<Entity, EntityRowMapper> {
 
 	@Override
 	public void deleteById(Object id) throws DataAccessException {
-		List<String> delete = new ArrayList<>();
+		StringBuilder delete = new StringBuilder();
 
-		delete.add("DELETE");
-		delete.add("FROM");
-		delete.add(Entity.TABLE_NAME);
-		delete.add("WHERE");
-		delete.add(Entity.ID + " = :" + Entity.ID);
+		delete.append("DELETE ");
+		delete.append("FROM ");
+		delete.append(Entity.TABLE_NAME);
+		delete.append(" WHERE ");
+		delete.append(Entity.ID + " = :" + Entity.ID);
 
 		MapSqlParameterSource params = new MapSqlParameterSource();
 		params.addValue(Entity.ID, id);
 
 		try {
-			setDeleteCount(namedjdbcTemplate.update(String.join(" ", delete), params));
+			setDeleteCount(namedjdbcTemplate.update(delete.toString(), params));
 		} catch (DataAccessException e) {
 			System.err.println(e.getMessage() + "\r\n" + e.getStackTrace());
 			throw e;
@@ -104,19 +101,19 @@ public class Repository extends AbstractRepository<Entity, EntityRowMapper> {
 	@Override
 	public void save(List<Entity> saveList) throws Exception {
 		SqlParameterSource[] batchParam = SqlParameterSourceUtils.createBatch(saveList);
-		List<String> save = new ArrayList<>();
+		StringBuilder save = new StringBuilder();
 		try {
 
-			save.add("INSERT INTO");
-			save.add(Entity.TABLE_NAME);
+			save.append("INSERT INTO ");
+			save.append(Entity.TABLE_NAME + " ");
 			setInsertSqlByMap(save);
 
-			save.add("ON DUPLICATE KEY");
-			save.add("UPDATE");
+			save.append("ON DUPLICATE KEY ");
+			save.append("UPDATE ");
 			setDuplicateUpdateSqlByMap(save);
-			save.add(";");
+			save.append(";");
 
-			int[] saveCounts = namedjdbcTemplate.batchUpdate(String.join(" ", save), batchParam);
+			int[] saveCounts = namedjdbcTemplate.batchUpdate(save.toString(), batchParam);
 			int saveCount = 0;
 			for (int count : saveCounts) {
 				saveCount += count;
@@ -136,170 +133,21 @@ public class Repository extends AbstractRepository<Entity, EntityRowMapper> {
 		MapSqlParameterSource params = new MapSqlParameterSource();
 
 		try {
-			List<String> save = new ArrayList<>();
+			StringBuilder save = new StringBuilder();
 			//データがなければインサート、あればアップデートをかける
-			save.add("INSERT INTO");
-			save.add(Entity.TABLE_NAME);
+			save.append("INSERT INTO ");
+			save.append(Entity.TABLE_NAME + " ");
 			setInsertSqlByMap(save, fieldNameAndValue, params);
-			save.add("ON DUPLICATE KEY");
-			save.add("UPDATE");
+			save.append("ON DUPLICATE KEY ");
+			save.append("UPDATE ");
 			setDuplicateUpdateSqlByMap(save, fieldNameAndValue);
-			save.add(";");
+			save.append(";");
 
-			String sql = String.join(" ", save);
-
-			setSaveCount(namedjdbcTemplate.update(sql, params));
+			setSaveCount(namedjdbcTemplate.update(save.toString(), params));
 		} catch (Exception e) {
 			System.err.println(e.getMessage() + "\r\n" + e.getStackTrace());
 			throw e;
 		}
-	}
-
-	/**
-	 * エンティティのstaticフィールド以外のフィールド名とその値を{@code Map<String, Object>}として返す
-	 * @param entity
-	 * @return key:フィールド名 value:そのフィールドの値 の{@code Map<String, Object>}
-	 * @throws Exception
-	 */
-	private Map<String, Object> getFieldNameAndValue(Entity entity) throws Exception {
-		try {
-			Map<String, Object> map = new LinkedHashMap<>();
-
-			Field[] fields = entity.getClass().getDeclaredFields();
-			for (Field field : fields) {
-				//privateフィールドへのアクセスを可能にする
-				field.setAccessible(true);
-				//staticフィールドの場合はスキップする
-				if (Modifier.isStatic(field.getModifiers())) {
-					continue;
-				}
-
-				String name = field.getName();
-				Object value = field.get(entity);
-				map.put(name, value);
-			}
-
-			return map;
-		} catch (IllegalArgumentException | IllegalAccessException e) {
-			System.err.println(e.getMessage() + "\r\n" + e.getStackTrace());
-			throw e;
-		}
-	}
-
-	/**
-	 * エンティティのフィールド名を{@code List<String>}にして返す
-	 * @return フィールド名をつめた{@code List<String>}
-	 * @throws Exception
-	 */
-	private List<String> getFieldNames() throws Exception {
-		try {
-			List<String> list = new ArrayList<>();
-
-			Field[] fields = new Entity().getClass().getDeclaredFields();
-			for (Field field : fields) {
-				//privateフィールドへのアクセスを可能にする
-				field.setAccessible(true);
-				// staticフィールドの場合はスキップする
-				if (Modifier.isStatic(field.getModifiers())) {
-					continue;
-				}
-				list.add(field.getName());
-			}
-
-			return list;
-		} catch (IllegalArgumentException e) {
-			System.err.println(e.getMessage() + "\r\n" + e.getStackTrace());
-			throw e;
-		}
-	}
-
-	/**
-	 * <pre>
-	 * key名 = VALUES(:key名), key名 = VALUES(:key名), .....
-	 * を作成する
-	 * </pre>
-	 * @param sqlList 作成中のSQL文
-	 * @param fieldNameAndValue key:カラム名 value:更新したい値
-	 */
-	private void setDuplicateUpdateSqlByMap(List<String> sqlList, Map<String, Object> fieldNameAndValue) {
-		List<String> updateSqlList = new ArrayList<>();
-		for (var entrySet : fieldNameAndValue.entrySet()) {
-			updateSqlList.add(entrySet.getKey() + " = VALUES(" + entrySet.getKey() + ")");
-		}
-		sqlList.add(String.join(", ", updateSqlList));
-	}
-
-	private void setDuplicateUpdateSqlByMap(List<String> sqlList) throws Exception {
-		List<String> updateSqlList = new ArrayList<>();
-		try {
-			for (String fieldName : getFieldNames()) {
-				updateSqlList.add(fieldName + " = VALUES(" + fieldName + ")");
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-		sqlList.add(String.join(", ", updateSqlList));
-	}
-
-	/**
-	 * <pre>
-	 * (key名, key名, key名, ...) VALUES (:key名, :key名, .....)
-	 * を作成する
-	 * </pre>
-	 * @param sqlList 作成中のSQL文
-	 * @param fieldNameAndValue key:カラム名 value:その値 のMap<String, Object> {@link #getFieldNameAndValue(Entity)}を使用して作成
-	 * @param params 作成中のパラメター
-	 */
-	private void setInsertSqlByMap(List<String> sqlList, Map<String, Object> fieldNameAndValue,
-			MapSqlParameterSource params) {
-		String[] columnArray = new String[fieldNameAndValue.size()];
-		String[] valueArray = new String[fieldNameAndValue.size()];
-
-		int index = 0;
-		for (var entrySet : fieldNameAndValue.entrySet()) {
-			String setKey = entrySet.getKey();
-			int idx = 0;
-			while (params.hasValue(setKey)) {
-				setKey = setKey + Integer.valueOf(idx).toString();
-				idx++;
-			}
-			params.addValue(setKey, entrySet.getValue());
-			columnArray[index] = entrySet.getKey();
-			valueArray[index] = ":" + setKey;
-			index++;
-		}
-
-		sqlList.add("( " + String.join(", ", columnArray) + " ) VALUES ( " + String.join(", ", valueArray) + " )");
-	}
-
-	/**
-	 * <pre>
-	 * (key名, key名, key名, ...) VALUES (:key名, :key名, .....)
-	 * を作成する
-	 * </pre>
-	 * @param sqlList 作成中のSQL文
-	 * @throws Exception
-	 */
-	private void setInsertSqlByMap(List<String> sqlList) throws Exception {
-		List<String> fieldNames;
-		try {
-			fieldNames = getFieldNames();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-		String[] columnArray = new String[fieldNames.size()];
-		String[] valueArray = new String[fieldNames.size()];
-
-		int index = 0;
-		for (String fieldName : fieldNames) {
-			columnArray[index] = fieldName;
-			valueArray[index] = ":" + fieldName;
-			index++;
-		}
-
-		sqlList.add("( " + String.join(", ", columnArray) + " ) VALUES ( " + String.join(", ", valueArray) + " )");
 	}
 
 }
